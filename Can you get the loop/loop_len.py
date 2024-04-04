99a41b576587d81ed4850921293493fc20baf5c0
Mykhailo_Ivasiuk <migelspainn@gmail.com>
2024-04-04 14:50:16 +0300

    basic code using list, working but too slow, needs optimization

--- Can you get the loop/loop_len.py
+++ Can you get the loop/loop_len.py
@@ -1,1 +1,11 @@
-
+class Node:
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+def loop_size(node):
+    loop_list = []
+    while node.next not in loop_list:
+            loop_list.append(node)
+            node = node.next
+    loop_list.append(node)
+    return loop_list.index(node) + 1 - loop_list.index(node.next)
