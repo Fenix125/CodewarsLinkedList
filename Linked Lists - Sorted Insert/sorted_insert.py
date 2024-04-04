84db87e6c1a2a1d19615cbedb7db78c3974be73d
Mykhailo_Ivasiuk <migelspainn@gmail.com>
2024-04-04 14:27:30 +0300

    idea of iteratin through nodes

--- Linked Lists - Sorted Insert/sorted_insert.py
+++ Linked Lists - Sorted Insert/sorted_insert.py
@@ -1,1 +1,15 @@
-
+def sorted_insert(head, data):
+    # Your code goes here.
+    # Make sure to return the head of the list.
+    while True:
+        head_dt = head.data
+        head_nxt = head.next
+        if head_dt < data < head_nxt:
+            node = Node(head_dt)
+            new_nxt = Node(data)
+            new_nxt.next = head.nxt
+            node.next = new_nxt
+            return node
+        else:
+            head.data = head_nxt
+            head.nxt = head_nxt.next
