f523c767bac60183edcee85610ee45bcfba3dbc6
Mykhailo_Ivasiuk <migelspainn@gmail.com>
2024-04-04 14:24:54 +0300

    basic code

--- Linked Lists - Move Node/lnk_lst_move_node.py
+++ Linked Lists - Move Node/lnk_lst_move_node.py
@@ -1,1 +1,9 @@
-
+def move_node(source, dest):
+    # Your code goes here.
+    # Remember to return the context.
+    if source == None:
+        raise IndexError
+    node_source = source.next
+    new_dest = Node(source.data)
+    new_dest.next = dest
+    return Context(node_source, new_dest)
