7e16f04db47d63150237e974dea17e3151be66b0
Mykhailo_Ivasiuk <migelspainn@gmail.com>
2024-04-04 14:38:11 +0300

    creating new head

--- Linked Lists - Remove Duplicates/remove_dublicates.py
+++ Linked Lists - Remove Duplicates/remove_dublicates.py
@@ -1,1 +1,18 @@
-
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+def remove_duplicates(head):
+    # Your code goes here.
+    # Remember to return the head of the list.
+    lst_nodes = []
+    while head.next != None:
+        lst_nodes.append(head.data)
+        head = head.next
+    lst_nodes += [head.data, None]
+    lst_nodes = list(set(lst_nodes))
+    lst_nodes.reverse()
+    head = None
+    for i in range(1, len(lst_nodes)):
+        head = push(head, lst_nodes[i])
+    return head
