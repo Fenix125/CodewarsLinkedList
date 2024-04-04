205ec9d12ab83f7fd113485d014b1517bee2676e
Mykhailo_Ivasiuk <migelspainn@gmail.com>
2024-04-04 14:42:42 +0300

    editing node class, and further developing func

--- Swap Node Pairs In Linked List/swap_node_pairs.py
+++ Swap Node Pairs In Linked List/swap_node_pairs.py
@@ -1,24 +1,23 @@
 import copy
 class Node:
-    def __init__(self, next=None):
+    def __init__(self, name, next=None):
         self.next = next
-    
+        self.name = name
+
 def push(head, data):
     # Your code goes here.
     node = Node(data)
     node.next = head
     return node
-
 def swap_pairs(head):
     if head == None or head.next == None:
         return head
-    head_next = head.next
-    head_next.next = head
+    temp_node = swap_pairs(head.next)
+    head.next.next = head
+    head.next = temp_node
+    head = temp_node
     return head
 
-
-c = Node()
-b = Node(c)
-a = Node(b)
-
-print(swap_pairs(a))
+c = Node('C')
+b = Node('B', c)
+a = Node('A', b)
