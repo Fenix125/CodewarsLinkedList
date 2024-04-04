1dde78e9b7ea5865f3e9fb22452399745f2b915d
Mykhailo_Ivasiuk <migelspainn@gmail.com>
2024-04-04 14:09:22 +0300

    made 1, 2, 3 func using push

--- Linked Lists - Push & BuildOneTwoThree/push_one_two_three.py
+++ Linked Lists - Push & BuildOneTwoThree/push_one_two_three.py
@@ -1,1 +1,9 @@
+def push(head, data):
+    # Your code goes here.
+    node = Node(data)
+    node.next = head
+    return node
 
+def build_one_two_three():
+    main_node = (push(push(push(None, 3), 2), 1))
+    return main_node
