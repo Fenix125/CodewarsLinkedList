4aeceb5907c670e692de6d51479bfec04cd2cc19
Mykhailo_Ivasiuk <migelspainn@gmail.com>
2024-04-04 14:34:49 +0300

    opening head

--- Linked Lists - Recursive Reverse/recursive_reverse.py
+++ Linked Lists - Recursive Reverse/recursive_reverse.py
@@ -1,1 +1,11 @@
+class Node(object):
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
 
+def reverse(head):
+    lst_nodes = []
+    while head.next != None:
+        lst_nodes.append(head.data)
+        head = head.next
+    lst_nodes += [head.data, None]
