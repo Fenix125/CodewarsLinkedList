cefb655be00acd0527c318481ad26714c51d7d16
Mykhailo_Ivasiuk <migelspainn@gmail.com>
2024-04-04 14:06:40 +0300

    made using recursion

--- Parse a linked list from a string/parse_lnk_lst_to_str.py
+++ Parse a linked list from a string/parse_lnk_lst_to_str.py
@@ -1,1 +1,8 @@
-
+def linked_list_from_string(s):
+    s = s.strip().split(' -> ')
+    i = 0
+    def node_recurs(i):
+        if s[i] == 'None':
+            return None
+        return Node(int(s[i]), node_recurs(i + 1))
+    return node_recurs(i)
