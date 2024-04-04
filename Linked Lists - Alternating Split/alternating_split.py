6e913f6a5b484243c4481b703f23a7ed6b7ae899
Mykhailo_Ivasiuk <migelspainn@gmail.com>
2024-04-04 14:47:52 +0300

    final code

--- Linked Lists - Alternating Split/alternating_split.py
+++ Linked Lists - Alternating Split/alternating_split.py
@@ -1,11 +1,18 @@
+class Node(object):
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+    
+class Context(object):
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+    
 def push(head, data):
     node = Node(data)
     node.next = head
     return node
-
 def alternating_split(head):
-    # Your code goes here.
-    # Remember to return the context.
     if head == None or head.next == None:
         raise ValueError
     lst_split = []
@@ -15,17 +22,20 @@ def alternating_split(head):
     lst_split += [head.data]
     list_first = []
     list_second = []
-    for i in range(1, len(lst_split), 2):
-        list_first.append(lst_split[i])
-        list_second.append(lst_split[i + 1])
+    for i in range(0, len(lst_split), 2):
+        try:
+            list_first.append(lst_split[i])
+            list_second.append(lst_split[i + 1])
+        except IndexError:
+            continue
     list_first.reverse()
     list_second.reverse()
     head1 = None
-    for i in range(1, len(list_first)):
-        head1 = push(head, list_first[i])
+    for i in range(0, len(list_first)):
+        head1 = push(head1, list_first[i])
     head2 = None
-    for i in range(1, len(list_second)):
-        head2 = push(head, list_second[i])
+    for i in range(0, len(list_second)):
+        head2 = push(head2, list_second[i])
     return Context(head1, head2)
 
 a = Node('a')
