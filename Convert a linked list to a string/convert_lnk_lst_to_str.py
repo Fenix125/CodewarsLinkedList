dd1926905b97c667b896b7ebe08cdb3cc3b91702
Mykhailo_Ivasiuk <migelspainn@gmail.com>
2024-04-04 13:59:25 +0300

    basic node unpacking

--- Convert a linked list to a string/convert_lnk_lst_to_str.py
+++ Convert a linked list to a string/convert_lnk_lst_to_str.py
@@ -1,1 +1,9 @@
-
+def stringify(node):
+    if not node:
+        return 'None'
+    stringa = ''
+    while node.next is not None:
+        stringa += str(node.data) + ' -> '
+        node = node.next
+    stringa += str(node.data) + ' -> ' + 'None'
+    return stringa
