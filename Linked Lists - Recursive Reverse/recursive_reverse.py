2c1ffce232f256b279ac51fa4593dcdf6e2cad2b
Mykhailo_Ivasiuk <migelspainn@gmail.com>
2024-04-04 14:35:44 +0300

    using previous task fucntion

--- Linked Lists - Recursive Reverse/recursive_reverse.py
+++ Linked Lists - Recursive Reverse/recursive_reverse.py
@@ -1,11 +1,15 @@
-class Node(object):
-    def __init__(self, data=None):
-        self.data = data
-        self.next = None
-
 def reverse(head):
+    if head == None:
+        return None
     lst_nodes = []
     while head.next != None:
         lst_nodes.append(head.data)
         head = head.next
-    lst_nodes += [head.data, None]
+    lst_nodes += [head.data]
+    lst_nodes.reverse()
+    lst_nodes += [None]
+    lst_nodes.reverse()
+    head = None
+    for i in range(1, len(lst_nodes)):
+        head = push(head, lst_nodes[i])
+    return head
